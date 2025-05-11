4d6d662154f4acb11c096b4b32c25ed4703f6820
geekp8 <82133404+geekp8@users.noreply.github.com>
2025-05-11 10:57:49 +0530

    Create Problem6.py

---
+++ Assignment_1/Problem6.py
@@ -0,0 +1,9 @@
+#check if a number is Positive,negative or zero
+number=int(input("Enter number:"))
+
+if(number>0):
+    print("Output: Positive number")
+elif(number<0):
+    print("Output:Negative number")
+else:
+    print("Output:Zero")    
