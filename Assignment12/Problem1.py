9b11c9be1a65a9338e6c2105dc126ec025b6223f
geekp8 <82133404+geekp8@users.noreply.github.com>
2025-06-01 13:35:28 +0530

    Update Problem1.py

--- Assignment12/Problem1.py
+++ Assignment12/Problem1.py
@@ -5,11 +5,11 @@ class Demo:
         self.no1=no1 #instance variables
         self.no2=no2 #instance variables
 
-        def Fun(self):
-            print("Fun method-> no1: {self.no1},no2:{self.no2},Value:{Demo.Value}")
+    def Fun(self):
+            print(f"Fun method-> no1: {self.no1},no2:{self.no2},Value:{Demo.Value}")
 
-        def gun(self):
-             print("Fun method-> no1: {self.no1},no2:{self.no2},Value:{Demo.Value}")
+    def Gun(self):
+             print(f"Fun method-> no1: {self.no1},no2:{self.no2},Value:{Demo.Value}")
 
 
 obj1=Demo(11,21)
