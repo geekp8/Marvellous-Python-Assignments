acc499b257941aad96bf3b1e12d42035647a554e
geekp8 <82133404+geekp8@users.noreply.github.com>
2025-06-01 13:30:07 +0530

    Create Problem1.py

---
+++ Assignment12/Problem1.py
@@ -0,0 +1,24 @@
+class Demo:
+    Value=0     #class variable
+    def __init__(self,no1,no2):
+        print("Inside constructor")
+        self.no1=no1 #instance variables
+        self.no2=no2 #instance variables
+
+        def Fun(self):
+            print("Fun method-> no1: {self.no1},no2:{self.no2},Value:{Demo.Value}")
+
+        def gun(self):
+             print("Fun method-> no1: {self.no1},no2:{self.no2},Value:{Demo.Value}")
+
+
+obj1=Demo(11,21)
+obj2=Demo(51,101)
+
+obj1.Fun()
+obj2.Fun()
+
+obj1.Gun()
+obj2.Gun()
+
+
