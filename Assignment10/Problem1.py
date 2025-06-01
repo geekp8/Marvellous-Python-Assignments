448bfdd14949dab27403b9537023fe9744e4f568
geekp8 <82133404+geekp8@users.noreply.github.com>
2025-06-01 12:24:41 +0530

    Create Problem1.py

---
+++ Assignment10/Problem1.py
@@ -0,0 +1,13 @@
+Square=lambda A:A*A
+
+def main():
+    print("Enter a number:")
+    no1=int(input())
+    Result=Square(no1)
+    print("Output is:",Result)
+    
+
+if __name__=="__main__":
+    main()
+
+
